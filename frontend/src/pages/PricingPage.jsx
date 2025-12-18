8e5e2f985258907cced0f933c847d4a471f20b95
Sebastian <sebastian.workvia@gmail.com>
2025-12-18 21:46:06 +0100

    Fix: Poprawka importu useAuthStore w PricingPage (named export)

--- frontend/src/pages/PricingPage.jsx
+++ frontend/src/pages/PricingPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { paymentService } from '../services/services'
-import useAuthStore from '../stores/authStore'
+import { useAuthStore } from '../stores/authStore'
 import './PricingPage.css'
 
 const PRICING_PLANS = {
